5856b92c875022489dea485c113a124ec57f7225
Kaio Luciano <kaiolucianocrispim@gmail.com>
2026-08-26 20:39:15 -0300

    Projeto_Csharp

---
+++ PrimeiroProjetoC#/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PrimeiroProjetoC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.pnlEntrar = new System.Windows.Forms.Panel();
+            this.pnlEntrar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(258, 152);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            this.lblEmail.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(91, 168);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(376, 20);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(258, 230);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(38, 13);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(91, 246);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(376, 20);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(222, 311);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(112, 23);
+            this.btnEntrar.TabIndex = 4;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // pnlEntrar
+            // 
+            this.pnlEntrar.Controls.Add(this.lblEmail);
+            this.pnlEntrar.Controls.Add(this.btnEntrar);
+            this.pnlEntrar.Controls.Add(this.txtEmail);
+            this.pnlEntrar.Controls.Add(this.txtSenha);
+            this.pnlEntrar.Controls.Add(this.lblSenha);
+            this.pnlEntrar.Location = new System.Drawing.Point(21, 12);
+            this.pnlEntrar.Name = "pnlEntrar";
+            this.pnlEntrar.Size = new System.Drawing.Size(583, 408);
+            this.pnlEntrar.TabIndex = 5;
+            this.pnlEntrar.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 450);
+            this.Controls.Add(this.pnlEntrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlEntrar.ResumeLayout(false);
+            this.pnlEntrar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Panel pnlEntrar;
+    }
+}
+
---
+++ PrimeiroProjetoC#/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiroProjetoC_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void CentralizarPainel()
+        {
+            //Mexe na parte esquerda do painel (o div 2 esta limitando nosso painel)
+            pnlEntrar.Left = (this.ClientSize.Width - pnlEntrar.Width) / 2;
+            //Mexe no topo do painel
+            pnlEntrar.Top = (this.ClientSize.Height - pnlEntrar.Height) / 2;
+        }
+    }
+}
